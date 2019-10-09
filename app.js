75154e047a3e2ece4f55c7b2a4d385067358d27d
Vivian Lo <yunyubee@gmail.com>
2019-10-09 13:57:50 +0800

    add mysql database

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./models')
+const Record = db.Record
+const User = db.User
+
 app.get('/', (req, res) => {
   res.send('hello')
 })
